915fea0cd63551ee837d5267d10d37bfa4e4f7de
Luca Hammer <lucahammer@gmail.com>
2016-01-02 17:13:37 +0100

    save id of last swiped tweet to start from there

--- default/main/control/tweets.py
+++ default/main/control/tweets.py
@@ -33,7 +33,7 @@ def tweets():
     tweets = api.home_timeline(count=30)
   tweetstring = '{"tweets":['
   for status in tweets:
-     tweetstring += '{"status":'+json.dumps(status.text)+',"account":"'+status.user.screen_name+'","avatar":"'+status.user.profile_image_url+'"},'
+     tweetstring += '{"status":'+json.dumps(status.text)+',"account":"'+status.user.screen_name+'","avatar":"'+status.user.profile_image_url+'", "id":"'+status.id_str+"'},'
      max_tweet_id = status.id_str
   tweetstring = tweetstring[:-1] #removing that last ',' to get valid json
   tweetstring += '], "max_tweet_id":"'+max_tweet_id+'"}'
